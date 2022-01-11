63b500099c46be6d1967a3b657d4dc6810dd021e
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-11 21:01:36 +0700

    Added media root to settings.py

--- personalWebsiteBackend/settings.py
+++ personalWebsiteBackend/settings.py
@@ -138,6 +138,10 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
