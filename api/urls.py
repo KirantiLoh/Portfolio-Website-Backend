616d1f326d2280966c143e5ee1298f11b086b4fb
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-12 16:55:19 +0700

    Added new function getBlogsSlugs

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.getRoutes, name='Routes'),
     path('projects', views.projects_view, name='Projects'),
     path('blogs', views.blogs_view, name="Blogs"),
+    path('blogs/slugs', views.getBlogSlugs, name="Get Blog's Slugs"),
     path('blogs/<slug:slug>', views.blog_view, name="Blog"),
 ]
