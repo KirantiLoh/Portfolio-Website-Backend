37d5efb934fe194d2982c82bd627d25e337a56db
Maurice Yang <mauriceyangmy28@gmail.com>
2022-02-03 09:21:38 +0700

    Fixed some bugs

--- api/views.py
+++ api/views.py
@@ -20,7 +20,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def projects_view(request):
-    projects = Project.objects.filter(show_projects = True)
+    projects = Project.objects.filter(show_project = True)
     serializer = ProjectSerializer(projects, many = True)
     return Response(serializer.data)
 
